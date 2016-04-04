0f06ff1e1e6fbe17516992ed1e0a6f56d77d4d27
Emile Choghi <emchoghi@gmail.com>
2016-04-03 18:31:29 -0700

    Create app.js

---
+++ ninety/js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+function detectMobile() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    console.log("mobile user");
+  }
+ else {
+    $(".container-fluid").css({'max-width': '90%'});
+    $(".container").css({'max-width': '850px'});
+  }
+}
+});
